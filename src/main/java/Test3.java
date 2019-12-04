3e6a5609c13245851ec312a6a76bb81bbaf79525
fuwenbing <1604556992@qq.com>
2019-12-04 10:41:08 +0800

    lastest

--- src/main/java/Test3.java
+++ src/main/java/Test3.java
@@ -8,6 +8,10 @@ public class Test3 {
     private Test2 t = new Test2();
 
     public static void main(String[] args) {
-        System.out.println("test3");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
