86da63583bbdb2155da69cc242f5422c17c00869
XiaodongFu <54094144+XiaodongFu@users.noreply.github.com>
2019-12-04 10:20:09 +0800

    Update Test2.java

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -6,6 +6,9 @@
  **/
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("讲出话来了");
+       
+ System.out.println("讲出话来了"); System.out.println("讲出话来了"); System.out.println("讲出话来了"); System.out.println("讲出话来了"); System.out.println("讲出话来了");
+        
+      
     }
 }
