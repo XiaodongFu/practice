3ea1721d1ae11ef662ec7c68652f323a3b5ab116
fuwenbing <1604556992@qq.com>
2019-12-04 10:02:02 +0800

    test1

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -8,7 +8,5 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git");
-        System.out.println("git");
-        System.out.println("git");
     }
 }
