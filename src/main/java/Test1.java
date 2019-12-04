76a36ec1b1545ca468d4f0dc22dafccee3b48eaf
fuwenbing <1604556992@qq.com>
2019-12-04 09:44:57 +0800

    test1

---
+++ src/main/java/Test1.java
@@ -0,0 +1,12 @@
+/**
+ * @program: practice
+ * @description: test1
+ * @author: Xiaodong
+ * @date: 2019-12-04 09:39
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println();
+    }
+}
