c9b03b2abe95007561179f470a4c321b9f8120e3
XiaodongFu <54094144+XiaodongFu@users.noreply.github.com>
2019-12-04 10:23:52 +0800

    Update Test2.java

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -7,7 +7,11 @@
 public class Test2 {
     public static void main(String[] args) {
        
- System.out.println("讲出话来了"); System.out.println("讲出话来了"); System.out.println("讲出话来了"); System.out.println("讲出话来了"); System.out.println("讲出话来了");
+        System.out.println("讲出话来了"); 
+        System.out.println("讲出话来了"); 
+        System.out.println("讲出话来了"); 
+        System.out.println("讲出话来了"); 
+        System.out.println("讲出话来了");
         
       
     }
