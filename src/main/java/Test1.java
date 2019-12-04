3c021daf0069346c2e138d8990c28cb3c095f686
XiaodongFu <54094144+XiaodongFu@users.noreply.github.com>
2019-12-04 10:05:48 +0800

    Update Test1.java

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -7,6 +7,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("git");
-        System.out.println("git");
+        System.out.println("git");System.out.println("git");System.out.println("git");System.out.println("git");System.out.println("git");
+       //测试
     }
 }
